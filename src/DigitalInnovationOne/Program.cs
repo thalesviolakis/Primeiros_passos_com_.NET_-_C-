524c13954b6f3d06970dbc7dd23880b29549b8da
Thales Violakis <contato@thalesviolakis.com>
2021-04-30 20:54:47 -0300

    Estudos iniciais sobre .NET

---
+++ src/DigitalInnovationOne/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 10;
+
+            for (int i = 0; i < numeroDeVezes; i++)
+            {
+                // Utilizando String Interpolation
+                Console.WriteLine($"Bem-vindo ao curso de .NET  {i}");
+            }
+
+        }
+    }
+}
